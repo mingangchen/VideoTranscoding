3510ed88bbfe683b0f413927043171e2e3ca096b
mingangchen <mgchen78@gmail.com>
2016-06-30 10:17:28 +0800

    Java example case for calling .exe file

---
+++ ExeFile.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+
+//Java 调用外部*.exe可执行文件
+public class ExeFile {
+	public static void main(String[] args){
+		System.out.println("Executing a exe file!");	
+		try {
+			Process process = null;
+			Runtime rt = Runtime.getRuntime();
+			process = rt.exec("c:\\windows\\system32\\notepad.exe");
+			System.out.println(rt.freeMemory());
+			System.out.println(rt.totalMemory());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Executing Error!");
+		}
+		System.out.println("Executing a NotePad Application!");
+	}
+}
+
+/****
+ * Process类是一个抽象类，封装了一个进程（即一个可执行程序）
+ * Runtime.getRuntime().exec("") 创建一个本机进程
+ * 每个Java Application都有一个Runtime类实例，使Java应用程序能够与其运行环境相连接。
+ ***/
